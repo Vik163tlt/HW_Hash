88f331725ba07f38fbcdf63ee3a134b207cb5347
VitaliyK <Vik163tlt@ya.ru>
2022-11-27 23:16:04 +0300

    Коллекции и множества

---
+++ src/Task_2_2.java
@@ -0,0 +1,42 @@
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+
+public class Task_2_2 {
+    private int number1;
+    private int number2;
+
+    public Task_2_2(int firstNumber, int secondNumber) {
+        this.number1 = firstNumber;
+        this.number2 = secondNumber;
+    }
+
+    public static Task_2_2 getTasks(List<Integer> list) {
+        Random random = new Random();
+        int firstNumber = random.nextInt(list.size())+2;
+        int secondNumber = random.nextInt(list.size())+2;
+        Task_2_2 task = new Task_2_2(firstNumber, secondNumber);
+        return task;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task_2_2 task = (Task_2_2) o;
+        return number1 == task.number1 && number2 == task.number2 || number2 == task.number1 && number1 == task.number2;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number1, number2);
+    }
+
+    @Override
+    public String toString() {
+        return number1 + "*" + number2 + "=?";
+
+    }
+}
+
